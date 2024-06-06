ba66bde20be9f7612bba049514d421633010e06a
VEGETAxHERE <feelthepowerofvegeta@gmail.com>
2024-06-06 14:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
       API_ID = int(getenv("API_ID", "20389440"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
       BOT_TOKEN = getenv("BOT_TOKEN", "......U")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001899353865:-1001899353865").replace("\n", " ").split(' '))
 
 
